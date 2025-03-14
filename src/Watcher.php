cbe1f78579a6e1a93c67d5465f2500508b9c2a9b
James Carter <jcarter@m-w.com>
2025-03-14 06:09:00 -0400

    refactor: improve code safety. sometimes  file might be missing and command might return as false

--- src/Watcher.php
+++ src/Watcher.php
@@ -77,7 +77,8 @@ class Watcher
                 }
             } else {
                 $ret = exec(sprintf('%s %s/vendor/hyperf/watcher/collector-reload.php %s', $this->option->getBin(), BASE_PATH, $file));
-                if ($ret['code'] === 0) {
+                $code = $ret['code'] ?? null;
+                if ($code === 0) {
                     $this->output->writeln('Class reload success.');
                 } else {
                     $this->output->writeln('Class reload failed.');
